f8d5f3aec187c1d9011739de2ac299d01da0245c
Albert Żóraw <albert.zoraw@xe05.ey.com>
2019-09-22 23:55:31 +0200

    -init default libgdx project (project Setup)

---
+++ desktop/src/com/zoraw/fractal/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zoraw.fractal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zoraw.fractal.FractalApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FractalApp(), config);
+	}
+}
