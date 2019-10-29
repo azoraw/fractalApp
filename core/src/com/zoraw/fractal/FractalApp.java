7da364be25ab933292d0a6cc85e92bffa2d51a8d
azoraw <al.gmail@gmail.com>
2019-10-29 20:57:36 +0100

    Draw simple mandelbrot set

--- core/src/com/zoraw/fractal/FractalApp.java
+++ core/src/com/zoraw/fractal/FractalApp.java
@@ -2,32 +2,72 @@ package com.zoraw.fractal;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FractalApp extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private static final int FRACTAL_WIDTH = 1500;
+    private static final int FRACTAL_HEIGHT = 900;
+
+    private static final int MAX_ITERATION = 600;
+
+    private SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render() {
+
+        Pixmap pmap = createPixelMap();
+        batch.begin();
+        batch.draw(new Texture(pmap), 0, 0);
+        batch.end();
+    }
+
+    private Pixmap createPixelMap() {
+        Pixmap pmap = new Pixmap(FRACTAL_WIDTH, FRACTAL_HEIGHT, Pixmap.Format.RGBA8888);
+        pmap.setColor(Color.WHITE);
+        pmap.fill();
+        pmap.setBlending(Pixmap.Blending.None);
+
+        for (int x = 0; x < FRACTAL_WIDTH; x++) {
+            for (int y = 0; y < FRACTAL_HEIGHT; y++) {
+                double[] normalizedPosition = normalizePosition(x, y);
+                double re = normalizedPosition[0];
+                double im = normalizedPosition[1];
+
+                double prevRe = 0;
+                double prevIm = 0;
+                double nextRe, nextIm;
+                int p;
+                for (p = 0; p < MAX_ITERATION; p++) {
+                    nextRe = prevRe * prevRe - prevIm * prevIm + re;
+                    nextIm = 2 * prevRe * prevIm + im;
+                    if (nextRe * nextRe + nextIm * nextIm > 4) {
+                        break;
+                    }
+                    prevRe = nextRe;
+                    prevIm = nextIm;
+                }
+                pmap.drawPixel(x, y, Color.rgba8888(p / MAX_ITERATION, p * 11 / MAX_ITERATION, p * 12 / MAX_ITERATION, 1));
+            }
+        }
+        return pmap;
+    }
+
+    private double[] normalizePosition(int x, int y) {
+        double[] normalizedPosition = new double[2];
+        normalizedPosition[0] = (x - (double) FRACTAL_WIDTH / 2) / (double) FRACTAL_WIDTH * 4;
+        normalizedPosition[1] = (y - (double) FRACTAL_HEIGHT / 2) / (double) FRACTAL_HEIGHT * 2;
+        return normalizedPosition;
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
--- desktop/src/com/zoraw/fractal/desktop/DesktopLauncher.java
+++ desktop/src/com/zoraw/fractal/desktop/DesktopLauncher.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.zoraw.fractal.FractalApp;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FractalApp(), config);
-	}
+    private static final int WIDTH = 1500;
+    private static final int HEIGHT = 900;
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = WIDTH;
+        config.height = HEIGHT;
+        new LwjglApplication(new FractalApp(), config);
+    }
 }
